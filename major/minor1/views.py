e834078e478bc923389a56ce3f44414eddac98ef
mohammed-7 <khanhuzaif@live.com>
2024-05-23 13:13:35 +0530

    added tailwind and made changes with jinja templates.

--- major/minor1/views.py
+++ major/minor1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_index(request):
+    return render(request,'app/all_index.html')
+
+def order(request):
+    return render(request,'app/order.html')
