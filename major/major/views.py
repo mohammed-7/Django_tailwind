1fef6374cc218ade02edc453bb648dcaff1b15a7
mohammed-7 <khanhuzaif@live.com>
2024-05-23 12:02:30 +0530

    first commit

---
+++ major/major/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+
+def home(request):
+    # return HttpResponse("Hello World!!")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("this is about page !")
