219e4595312512382419d837d9c3296785bc69f4
mohammed-7 <khanhuzaif@live.com>
2024-05-24 21:00:01 +0530

    made changes to models.py | admin.py | settings.py

--- major/minor1/models.py
+++ major/minor1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class car(models.Model):
+    CAR_TYPE_CHOICE = [
+        ('HB','Hatch back'),
+        ('Lx','luxury'),
+        ('Cp','Coupe'),
+        ('Ul','Ultra luxury'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'minor1/')
+    date_added = models.DateField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=CAR_TYPE_CHOICE)
+    
