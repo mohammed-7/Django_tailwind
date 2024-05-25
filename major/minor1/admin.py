cefdf6c5bdd0fc461ac1a86cc344164db2681c5e
mohammed-7 <khanhuzaif@live.com>
2024-05-25 19:20:07 +0530

    configd connection btwn db & models

--- major/minor1/admin.py
+++ major/minor1/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import CarVariety
 
 # Register your models here.
-from .models import CarVariety
+#through this admin model we can see any model onto the admin panel.
+ 
 
 admin.site.register(CarVariety)
--- major/minor1/views.py
+++ major/minor1/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import CarVariety
+
 # Create your views here.
+
 def all_index(request):
-    
+    cars = CarVariety.objects.all()    
     return render(request,'app/all_index.html',{'cars':cars})
 
 def order(request):
